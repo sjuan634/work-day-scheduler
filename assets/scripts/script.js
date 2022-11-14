22070903b33efcdef0e6fe95b4467bba516d9e68
sanchezju <sjuan634@yahoo.com>
2022-11-13 19:32:00 -0800

    add function to create HTML for hour divs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,34 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  const mainContainer = document.querySelector('#main-container');
+  const currentDate = dayjs().format('dddd, MMMM D');
+  const currentTime = dayjs().format('hA');
+
+  function createHourBlock() {
+    let hourBlockDiv = document.createElement('div');
+    hourBlockDiv.classList.add('row', 'time-block');
+    let hourDiv = document.createElement('div');
+    hourDiv.classList.add('col-2', 'col-md-1', 'hour', 'text-center', 'py-3');
+    let textArea = document.createElement('textarea');
+    textArea.classList.add('col-8', 'col-md-10', 'description');
+    textArea.setAttribute('row', '3');
+    let saveButton = document.createElement('button');
+    saveButton.classList.add('btn', 'saveBtn', 'col-2', 'col-md-1');
+    saveButton.setAttribute('aria-label', 'save');
+    let saveIcon = document.createElement('i');
+    saveIcon.classList.add('fas', 'fa-save');
+    saveIcon.setAttribute('aria-hidden', 'true');
+    
+    saveButton.appendChild(saveIcon);
+    hourBlockDiv.appendChild(hourDiv);
+    hourBlockDiv.appendChild(textArea);
+    hourBlockDiv.appendChild(saveButton);
+
+    return(hourBlockDiv);
+  }
+  
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
